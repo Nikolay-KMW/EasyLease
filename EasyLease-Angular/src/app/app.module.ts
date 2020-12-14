f20aa67ff9fc69a1b7c301ffc1b31f2302853d70
Nikolay <Nikolay.KMW@gmail.com>
2020-12-14 22:49:28 +0200

    Added new template styles and installed Bootstrap SCSS package.

--- EasyLease-Angular/src/app/app.module.ts
+++ EasyLease-Angular/src/app/app.module.ts
@@ -3,11 +3,17 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
